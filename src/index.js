af4d98267d7d90c6635abcecd7828d61fd02752c
Huracan22505 <nokia2611@mail.ru>
2021-09-13 11:06:28 +0300

    feat: add SASS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "redux/store";
 import "modern-normalize/modern-normalize.css";
-import "./index.css";
+import "./index.scss";
 
 import App from "components/App/";
 
