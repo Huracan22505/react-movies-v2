3056c98aaca5ba6e42f52783e55e1ed8d9dbfbae
Huracan22505 <nokia2611@mail.ru>
2021-10-26 11:24:26 +0300

    refactor: update store

---
+++ src/common/interfaces/index.ts
@@ -0,0 +1,21 @@
+export interface Action {
+  type: string;
+  payload: any;
+  meta: any;
+}
+
+// export interface Movie {
+//   id: string;
+//   name: string;
+//   cover: string;
+//   description: string;
+// }
+
+// export interface Ticket {
+//   data: {
+//     name: string;
+//     id: string;
+//     image: string;
+//     used: boolean;
+//   };
+// }
