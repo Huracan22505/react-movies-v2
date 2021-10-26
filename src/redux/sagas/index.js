06740d4cb5091298eaaf6237b2bcacdb8e7cf251
Huracan22505 <nokia2611@mail.ru>
2021-10-26 11:10:28 +0300

    feat: add redux-saga

---
+++ src/redux/sagas/index.js
@@ -0,0 +1,33 @@
+import { MoviesApi } from 'modules/movies/services/movies-api.service';
+import { put, call, takeLatest, all } from 'redux-saga/effects';
+// import { SagaIterator } from '@redux-saga/types';
+import {
+  fetchMovieByIdSuccess,
+  fetchMoviesSuccess,
+} from '../actions/movies-actions.js';
+import { Actions } from '../actions/actionsTypes';
+
+export function* fetchMoviesSaga({ payload }) {
+  try {
+    const movies = yield call(MoviesApi.getMovies, payload);
+    yield put(fetchMoviesSuccess(movies));
+  } catch (error) {
+    throw Error(error.message);
+  }
+}
+
+export function* fetchMovieByIdSaga(id) {
+  try {
+    const movie = yield call(MoviesApi.getMovieById, id);
+    yield put(fetchMovieByIdSuccess(movie));
+  } catch (error) {
+    throw Error(error.message);
+  }
+}
+
+export function* rootSaga() {
+  yield all([
+    takeLatest(Actions.FetchMoviesRequest, fetchMoviesSaga),
+    takeLatest(Actions.fetchMovieByIdRequest, fetchMovieByIdSaga),
+  ]);
+}
