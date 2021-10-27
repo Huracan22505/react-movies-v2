da154930f24d93fe027e85a0d624f134bc3b2b9e
Huracan22505 <nokia2611@mail.ru>
2021-10-27 13:09:37 +0300

    feat: movies page TS

--- src/view/MoviesPage/MoviesPage.tsx
+++ src/view/MoviesPage/MoviesPage.tsx
@@ -1,15 +1,16 @@
-import s from "./MoviesPage.module.css";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useLocation, useHistory } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import s from './MoviesPage.module.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useLocation, useHistory } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import MoviesList from "components/MoviesList";
+import MoviesList from 'components/MoviesList';
+import { IMovie } from 'common/interfaces';
 
 function MoviesPage() {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState<IMovie[]>([]);
 
   const location = useLocation();
   const history = useHistory();
@@ -18,24 +19,24 @@ function MoviesPage() {
     if (location.search) fetch(location.search.slice(7));
   }, [location.search]);
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.currentTarget.value.toLowerCase());
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     fetch(query);
   };
 
-  const fetch = async (query) => {
+  const fetch = async (query: string) => {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=cac5c08a938bff767b15f4beaa543e5a&language=en-US&page=1&include_adult=false`
+      `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=cac5c08a938bff767b15f4beaa543e5a&language=en-US&page=1&include_adult=false`,
     );
 
     if (response.data.results.length < 1) {
-      toast.error("Wrong request. No results!");
-      setQuery("");
+      toast.error('Wrong request. No results!');
+      setQuery('');
       return;
     }
 
