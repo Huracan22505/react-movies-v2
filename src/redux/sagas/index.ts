58d6554ed9fcdd4c447f82e86a2e2e78effa0678
Huracan22505 <nokia2611@mail.ru>
2021-10-27 13:08:42 +0300

    feat: sagas TS

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,31 +1,37 @@
 import { MoviesApi } from 'modules/movies/services/movies-api.service';
 import { put, call, takeLatest, all } from 'redux-saga/effects';
-// import { SagaIterator } from '@redux-saga/types';
+import { SagaIterator } from '@redux-saga/types';
 import {
   fetchMovieByIdSuccess,
   fetchMoviesSuccess,
 } from '../actions/movies-actions.js';
 import { Actions } from '../actions/actionsTypes';
+import { IMovie } from 'common/interfaces/index.js';
 
-export function* fetchMoviesSaga({ payload }) {
+interface IParams {
+  payload: number;
+  type: string;
+}
+
+export function* fetchMoviesSaga({ payload }: IParams): SagaIterator {
   try {
     const movies = yield call(MoviesApi.getMovies, payload);
     yield put(fetchMoviesSuccess(movies));
   } catch (error) {
-    throw Error(error.message);
+    console.log('Error fetchMoviesSaga');
   }
 }
 
-export function* fetchMovieByIdSaga(id) {
+export function* fetchMovieByIdSaga(id: string | any): SagaIterator {
   try {
-    const movie = yield call(MoviesApi.getMovieById, id);
+    const movie: IMovie = yield call(MoviesApi.getMovieById, id);
     yield put(fetchMovieByIdSuccess(movie));
   } catch (error) {
-    throw Error(error.message);
+    console.log('Error fetchMovieByIdSaga');
   }
 }
 
-export function* rootSaga() {
+export function* rootSaga(): SagaIterator {
   yield all([
     takeLatest(Actions.FetchMoviesRequest, fetchMoviesSaga),
     takeLatest(Actions.FetchMovieByIdRequest, fetchMovieByIdSaga),
